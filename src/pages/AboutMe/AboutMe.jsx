acd084d0d11b2d53075911a4e22770957f0750f5
Zubayr2110 <webproject2110@gmail.com>
2023-11-26 09:02:35 +0500

    Update

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,14 +1,12 @@
 import Header from '../../components/Header/Header'
 import './AboutMe.css'
 import AboutMep from './AboutMep'
-import Footer from '../../components/Footer/Footer.jsx'
 
 export default function AboutMe() {
   return (
     <div className='contactContent'>
       <Header />
       <AboutMep />
-      <Footer />
       
     </div>
   )
